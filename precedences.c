8c7b69c007a72e69b74e5080cdf6c35bd9da0950
kambenj <kamil.benjelloun@edu.ece.fr>
2023-11-26 21:58:29 +0100

    precedences

--- precedences.c
+++ precedences.c
@@ -7,17 +7,8 @@
 #include <stdlib.h>
 
 int main() {
-    //tableau
-
-    int tab[5] = {1, 2, 3, 4, 5};
-    int *p = tab;
-    int *q = tab + 2;
-    int *r = tab + 4;
-    printf("%d\n", *p);
-
-    printf("%d\n", *q);
-    printf("%d\n", *r);
-    printf("%d\n", *p + 1);
-    printf("%d\n", *q + 1);
-
+    //structure
+    int a = 1, b = 2, c = 3, d = 4;
+    int x = a + b * c / d;
+    printf("x = %d\n", x);
 }
