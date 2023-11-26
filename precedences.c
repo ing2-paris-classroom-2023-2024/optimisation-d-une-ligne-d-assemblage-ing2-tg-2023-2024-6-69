550983301bb9bdf3f6fadd9261f2e705762adbc5
kambenj <kamil.benjelloun@edu.ece.fr>
2023-11-26 21:59:00 +0100

    precedences

--- precedences.c
+++ precedences.c
@@ -7,8 +7,6 @@
 #include <stdlib.h>
 
 int main() {
-    //structure
-    int a = 1, b = 2, c = 3, d = 4;
-    int x = a + b * c / d;
-    printf("x = %d\n", x);
+    printf("Hello, World!\n");
+    return 0;
 }
