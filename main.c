bc095d38b62e7646460afc8b6da3d83e7a4f4cca
antoi <antoine.jessel@edu.ece.fr>
2023-12-03 19:57:42 +0100

    menu

--- main.c
+++ main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+//#include "precedences.c"
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int main(){
+    int choix;
+    printf("Selectionnez une option :\n1- Exclusion\n2- Precedence et Temps de Cycle\n3- Exclusion et Precedence\n4- Exclusion et Temps de Cycle\n5- Les 3 a la fois\n");
+    scanf("%d", &choix);
+    switch (choix) {
+        case 1:
+            printf("1. Exclusion\n");
+            break;
+        case 2:
+            printf("2. Precedence et Temps de Cycle\n");
+            //precedences();
+            break;
+        case 3:
+            printf("3. Exclusion et Precedence\n");
+            break;
+        case 4:
+            printf("4. Exclusion et Temps de Cycle\n");
+            break;
+        case 5:
+            printf("5. Les 3 a la fois\n");
+            break;
+    }
 }
