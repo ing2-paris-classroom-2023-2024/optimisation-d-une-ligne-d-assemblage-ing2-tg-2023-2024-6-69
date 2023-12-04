25f808f95d42647e21e46875cac0808d5a416274
antoi <antoine.jessel@edu.ece.fr>
2023-12-04 14:03:33 +0100

    menu maj

--- main.c
+++ main.c
@@ -12,10 +12,30 @@ int main() {
     int choix;
     scanf("%d", &choix);
 
-    // applique contrainte
-    graphe->contrainteExclusion = (choix == 1 || choix == 3);
-    graphe->contrainteTemps = (choix == 2 || choix == 3);
-    graphe->contraintePrecedence = (choix == 2 || choix == 3);
+    // choix contraintes
+    switch (choix) {
+        case 1:
+            graphe->contrainteExclusion = 1;
+            graphe->contrainteTemps = 0;
+            graphe->contraintePrecedence = 0;
+            break;
+
+        case 2:
+            graphe->contrainteExclusion = 0;
+            graphe->contrainteTemps = 1;
+            graphe->contraintePrecedence = 1;
+            break;
+
+        case 3:
+            graphe->contrainteExclusion = 1;
+            graphe->contrainteTemps = 1;
+            graphe->contraintePrecedence = 1;
+            break;
+
+        default:
+            printf("Option invalide.\n");
+            return 1;
+    }
 
     //lecture fichiers
     lireRelationsPrecedence(graphe, "precedences.txt");
