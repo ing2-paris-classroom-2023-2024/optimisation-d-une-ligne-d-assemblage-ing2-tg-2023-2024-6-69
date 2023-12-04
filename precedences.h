d3a247a11824f4dd02eea31def05123a2e8ee1f5
antoi <antoine.jessel@edu.ece.fr>
2023-12-04 13:14:26 +0100

    precedences

--- precedences.h
+++ precedences.h
@@ -1,32 +1,10 @@
 #ifndef OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_6_69_PRECEDENCES_H
 #define OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_6_69_PRECEDENCES_H
 
-typedef struct Noeud {
-    int sommet;
-    struct Noeud* suivant;
-} Noeud;
+#include "graphe.h"
 
-typedef struct {
-    Noeud** tableau;
-    int* degreEntrant;
-    struct Affectation {
-        int operation;
-        int station;
-    } affectations;
-    int sommets;
-    int nombreAffectations;
-} Graphep;
+//lire relations précédence et les intégrer dans le graphe
+void lireRelationsPrecedence(Graphe* graphe, const char* nomFichier);
 
-Graphep creerGraphe(int sommets);
-void ajouterArc(Graphep* graphep, int src, int dest);
-void imprimerGraphep(Graphep* graphep);
-void libererGraphep(Graphep* graphep);
-Graphep* construireGrapheDepuisFichier(const char* nomFichier);
-double recupererDureeOperation(int numeroOperation);
-
-void ajouterOperation(Graphep* graphe, int station, int sommet);
-void retirerOperation(Graphep* graphe, int station, int sommet);
-int trouverStationDisponible(Graphep* graphe, int operation, int tempsCycle);
-void verifierContrainteTempsCycle(Graphep* graphe, const char* fichierTempsCycle);
 
 #endif //OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_6_69_PRECEDENCES_H
