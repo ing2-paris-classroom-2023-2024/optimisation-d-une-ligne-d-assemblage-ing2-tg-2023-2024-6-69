e68a0f0b08a9316aa03374a02b6a42add105d310
antoi <antoine.jessel@edu.ece.fr>
2023-12-04 13:24:03 +0100

    .

--- temps_cycle.h
+++ temps_cycle.h
@@ -1,11 +1,9 @@
-//
-// Created by antoi on 04/12/2023.
-//
+
 
 #ifndef OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_6_69_TEMPS_CYCLE_H
 #define OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_6_69_TEMPS_CYCLE_H
 
-#include "graph.h"
+#include "graphe.h"
 float lireDureeCycle(const char* nomFichier);
 //algorithme de parcours en largeur
 void bfs(Graphe* graphe);
