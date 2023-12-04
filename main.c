901635c0b93e558f85cbb70e82745939ca8b29d0
antoi <antoine.jessel@edu.ece.fr>
2023-12-04 13:20:00 +0100

    menu nouveau

--- main.c
+++ main.c
@@ -1,30 +1,36 @@
+#include "Graph.h"
+#include "precedences.h"
+#include "temps_cycle.h"
+#include "exclusions.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "header.h"
 
+int main() {
 
-int main(){
+    //creer graphe
+    Graphe* graphe = creerGraphe(MAX_OPERATIONS);
+
+    //menu
+    printf("Choisissez les contraintes à appliquer :\n");
+    printf("1: Exclusion seulement\n");
+    printf("2: Temps de cycle et précédence\n");
+    printf("3: Toutes les contraintes\n");
     int choix;
-    printf("Selectionnez une option :\n1- Exclusion\n2- Precedence et Temps de Cycle\n3- Exclusion et Precedence\n4- Exclusion et Temps de Cycle\n5- Les 3 a la fois\n");
     scanf("%d", &choix);
-    switch (choix) {
-        case 1:
-            printf("1. Exclusion\n");
-            //exclusions();
-            break;
-        case 2:
-            printf("2. Precedence et Temps de Cycle\n");
-            //precedences();
-            break;
-        case 3:
-            printf("3. Exclusion et Precedence\n");
-            break;
-        case 4:
-            printf("4. Exclusion et Temps de Cycle\n");
-            break;
-        case 5:
-            printf("5. Les 3 a la fois\n");
-            break;
-    }
+
+    // Applique les contraintes en fonction du choix de l'utilisateur
+    graphe->contrainteExclusion = (choix == 1 || choix == 3);
+    graphe->contrainteTemps = (choix == 2 || choix == 3);
+    graphe->contraintePrecedence = (choix == 2 || choix == 3);
+
+    //lire fichies
+    lireRelationsPrecedence(graphe, "C:/Users/nicol/OneDrive/Bureau/precedences.txt");
+    lireTempsOperations(graphe, "C:/Users/nicol/OneDrive/Bureau/operations.txt");
+    lirePairesExclusion(graphe, "C:/Users/nicol/OneDrive/Bureau/exclusions.txt");
+    graphe->tempsCycle = lireDureeCycle("C:/Users/nicol/OneDrive/Bureau/temps_cycle.txt");
+    bool visite[MAX_OPERATIONS] = {false};
+
+    //parcourir graphe
+    bfs(graphe);
+
+    return 0;
 }
